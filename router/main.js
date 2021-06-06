ca0380b3dd8932afb91fb386096e256af4532291
klmlboom <klmlboom1013@gmail.com>
2021-06-06 22:43:14 +0900

    ll

--- router/main.js
+++ router/main.js
@@ -6,4 +6,14 @@ module.exports = function(app)
       let d_ch2_1 = 'chapters/chapter-02/01-declaring-variables';
       app.get('/ch2-01',function(req,res){ res.render(d_ch2_1 + '/01-const.html'); });
       app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
+      app.get('/ch2-02',function(req,res){ res.render(d_ch2_1 + '/02-const.html'); });
 }
