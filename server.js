ffbb29296320ea047891d46c4fae68e651cbec04
klmlboom <klmlboom1013@gmail.com>
2021-06-10 23:56:24 +0900

    chapter router 분리

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
-var router = require('./router/main')(app);
+const express = require('express');
+const app = express();
+
+const main = require('./router/main')(app);
+const chapter2 = require('./router/chapter02')(app);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(80, function(){
-    console.log("Express server has started on port 3000")
-})
+var server = app.listen(80, () => {
+    console.log("Express server has started on port 80");
+});
+
 app.use(express.static('public'));
