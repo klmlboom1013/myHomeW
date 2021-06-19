3085b5fded943a152cbb6f3969b5f97cbc6ed511
klmlboom <klmlboom1013@gmail.com>
2021-06-19 18:49:15 +0900

    ejs test

--- router/main.js
+++ router/main.js
@@ -1,4 +1,8 @@
-module.exports = (app) => {
-      app.get('/'       , (req,res) => { res.render('index.html'); });
-      app.get('/about'  , (req,res) => { res.render('about.html'); });
+module.exports = (app, fs) => {
+      app.get('/',function(req,res){
+            res.render('index', {
+                  title: "MY HOMEPAGE",
+                  length: 7
+            });
+        });
 };
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const app = express();
+/*
+[210619]
+npn install body-parser
 
-const main = require('./router/main')(app);
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require("fs");
+
+const app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(80, () => {
-    console.log("Express server has started on port 80");
+const server = app.listen(80, () => {
+    console.log("Express server has started on port 80 ***");
 });
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
+const router = require('./router/main')(app, fs);
